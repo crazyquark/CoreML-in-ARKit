bdbbc0e523ad6a7331b393da957a08cd7f860f96
crazyquark <cristian.sandu@gmail.com>
2017-10-02 13:05:19 +0300

    initial cleanup

--- CoreML in ARKit/ViewController.swift
+++ CoreML in ARKit/ViewController.swift
@@ -44,20 +44,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         //////////////////////////////////////////////////
         // Tap Gesture Recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognize:)))
-        view.addGestureRecognizer(tapGesture)
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognize:)))
+//        view.addGestureRecognizer(tapGesture)
         
         //////////////////////////////////////////////////
         
         // Set up Vision Model
-        guard let selectedModel = try? VNCoreMLModel(for: Inceptionv3().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
-            fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/ . Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
-        }
+//        guard let selectedModel = try? VNCoreMLModel(for: Inceptionv3().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
+//            fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/ . Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
+//        }
         
         // Set up Vision-CoreML Request
-        let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
-        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
-        visionRequests = [classificationRequest]
+//        let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
+//        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
+//        visionRequests = [classificationRequest]
         
         // Begin Loop to Update CoreML
         loopCoreMLUpdate()
@@ -102,65 +102,65 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     // MARK: - Interaction
     
-    @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
-        // HIT TEST : REAL WORLD
-        // Get Screen Centre
-        let screenCentre : CGPoint = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
-        
-        let arHitTestResults : [ARHitTestResult] = sceneView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
-        
-        if let closestResult = arHitTestResults.first {
-            // Get Coordinates of HitTest
-            let transform : matrix_float4x4 = closestResult.worldTransform
-            let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-            
-            // Create 3D Text
-            let node : SCNNode = createNewBubbleParentNode(latestPrediction)
-            sceneView.scene.rootNode.addChildNode(node)
-            node.position = worldCoord
-        }
-    }
-    
-    func createNewBubbleParentNode(_ text : String) -> SCNNode {
-        // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
-        
-        // TEXT BILLBOARD CONSTRAINT
-        let billboardConstraint = SCNBillboardConstraint()
-        billboardConstraint.freeAxes = SCNBillboardAxis.Y
-        
-        // BUBBLE-TEXT
-        let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
-        var font = UIFont(name: "Futura", size: 0.15)
-        font = font?.withTraits(traits: .traitBold)
-        bubble.font = font
-        bubble.alignmentMode = kCAAlignmentCenter
-        bubble.firstMaterial?.diffuse.contents = UIColor.orange
-        bubble.firstMaterial?.specular.contents = UIColor.white
-        bubble.firstMaterial?.isDoubleSided = true
-        // bubble.flatness // setting this too low can cause crashes.
-        bubble.chamferRadius = CGFloat(bubbleDepth)
-        
-        // BUBBLE NODE
-        let (minBound, maxBound) = bubble.boundingBox
-        let bubbleNode = SCNNode(geometry: bubble)
-        // Centre Node - to Centre-Bottom point
-        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
-        // Reduce default text size
-        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
-        
-        // CENTRE POINT NODE
-        let sphere = SCNSphere(radius: 0.005)
-        sphere.firstMaterial?.diffuse.contents = UIColor.cyan
-        let sphereNode = SCNNode(geometry: sphere)
-        
-        // BUBBLE PARENT NODE
-        let bubbleNodeParent = SCNNode()
-        bubbleNodeParent.addChildNode(bubbleNode)
-        bubbleNodeParent.addChildNode(sphereNode)
-        bubbleNodeParent.constraints = [billboardConstraint]
-        
-        return bubbleNodeParent
-    }
+//    @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
+//        // HIT TEST : REAL WORLD
+//        // Get Screen Centre
+//        let screenCentre : CGPoint = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
+//
+//        let arHitTestResults : [ARHitTestResult] = sceneView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
+//
+//        if let closestResult = arHitTestResults.first {
+//            // Get Coordinates of HitTest
+//            let transform : matrix_float4x4 = closestResult.worldTransform
+//            let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+//
+//            // Create 3D Text
+//            let node : SCNNode = createNewBubbleParentNode(latestPrediction)
+//            sceneView.scene.rootNode.addChildNode(node)
+//            node.position = worldCoord
+//        }
+//    }
+//
+//    func createNewBubbleParentNode(_ text : String) -> SCNNode {
+//        // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
+//
+//        // TEXT BILLBOARD CONSTRAINT
+//        let billboardConstraint = SCNBillboardConstraint()
+//        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+//
+//        // BUBBLE-TEXT
+//        let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
+//        var font = UIFont(name: "Futura", size: 0.15)
+//        font = font?.withTraits(traits: .traitBold)
+//        bubble.font = font
+//        bubble.alignmentMode = kCAAlignmentCenter
+//        bubble.firstMaterial?.diffuse.contents = UIColor.orange
+//        bubble.firstMaterial?.specular.contents = UIColor.white
+//        bubble.firstMaterial?.isDoubleSided = true
+//        // bubble.flatness // setting this too low can cause crashes.
+//        bubble.chamferRadius = CGFloat(bubbleDepth)
+//
+//        // BUBBLE NODE
+//        let (minBound, maxBound) = bubble.boundingBox
+//        let bubbleNode = SCNNode(geometry: bubble)
+//        // Centre Node - to Centre-Bottom point
+//        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
+//        // Reduce default text size
+//        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+//
+//        // CENTRE POINT NODE
+//        let sphere = SCNSphere(radius: 0.005)
+//        sphere.firstMaterial?.diffuse.contents = UIColor.cyan
+//        let sphereNode = SCNNode(geometry: sphere)
+//
+//        // BUBBLE PARENT NODE
+//        let bubbleNodeParent = SCNNode()
+//        bubbleNodeParent.addChildNode(bubbleNode)
+//        bubbleNodeParent.addChildNode(sphereNode)
+//        bubbleNodeParent.constraints = [billboardConstraint]
+//
+//        return bubbleNodeParent
+//    }
     
     // MARK: - CoreML Vision Handling
     
@@ -177,42 +177,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
-    func classificationCompleteHandler(request: VNRequest, error: Error?) {
-        // Catch Errors
-        if error != nil {
-            print("Error: " + (error?.localizedDescription)!)
-            return
-        }
-        guard let observations = request.results else {
-            print("No results")
-            return
-        }
-        
-        // Get Classifications
-        let classifications = observations[0...1] // top 2 results
-            .flatMap({ $0 as? VNClassificationObservation })
-            .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
-            .joined(separator: "\n")
-        
-        
-        DispatchQueue.main.async {
-            // Print Classifications
-            print(classifications)
-            print("--")
-            
-            // Display Debug Text on screen
-            var debugText:String = ""
-            debugText += classifications
-            self.debugTextView.text = debugText
-            
-            // Store the latest prediction
-            var objectName:String = "…"
-            objectName = classifications.components(separatedBy: "-")[0]
-            objectName = objectName.components(separatedBy: ",")[0]
-            self.latestPrediction = objectName
-            
-        }
-    }
+//    func classificationCompleteHandler(request: VNRequest, error: Error?) {
+//        // Catch Errors
+//        if error != nil {
+//            print("Error: " + (error?.localizedDescription)!)
+//            return
+//        }
+//        guard let observations = request.results else {
+//            print("No results")
+//            return
+//        }
+//
+//        // Get Classifications
+//        let classifications = observations[0...1] // top 2 results
+//            .flatMap({ $0 as? VNClassificationObservation })
+//            .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
+//            .joined(separator: "\n")
+//
+//
+//        DispatchQueue.main.async {
+//            // Print Classifications
+//            print(classifications)
+//            print("--")
+//
+//            // Display Debug Text on screen
+//            var debugText:String = ""
+//            debugText += classifications
+//            self.debugTextView.text = debugText
+//
+//            // Store the latest prediction
+//            var objectName:String = "…"
+//            objectName = classifications.components(separatedBy: "-")[0]
+//            objectName = objectName.components(separatedBy: ",")[0]
+//            self.latestPrediction = objectName
+//
+//        }
+//    }
     
     func updateCoreML() {
         ///////////////////////////
